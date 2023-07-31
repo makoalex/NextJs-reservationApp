1a6aa5b4ff824f5b55599531127c8106c7a4d90f
Alexandra Macovei <macovei.a2@gmail.com>
2023-07-31 09:51:25 +0200

    C2-> finshed setting up Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2> welcome to nextjs!</h2>
+      <h2 className='text-blue-400 text-7xl'>
+         welcome to nextjs!</h2>
     </main>
-  )
+  );
 }
