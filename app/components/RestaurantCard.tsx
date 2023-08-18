2cac9d857baa3927f2aae82833b6b1c83af10be1
Alexandra Macovei <macovei.a2@gmail.com>
2023-08-18 12:41:56 +0200

    [fix] displaying all restaurants programmtically

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import { RestaurantCardType } from '../page'
+import Price from './Price'
 
 interface Props {
   restaurant: RestaurantCardType
@@ -8,22 +9,22 @@ interface Props {
 
 export default function RestaurantCard({restaurant}:Props) {
   return (
-    <div className="w-64 h-70 m-3 cursor-pointer  rounded border overflow-hidden">
+    <div className="w-64 h-70 m-auto px-auto  cursor-pointer flex grow-1  rounded border overflow-hidden">
     <Link href={`/restaurant/${restaurant.slug}`}>
-      <img
+      <img 
         src={restaurant.mainImage}
         alt=""
-        className="mr-2"
+        className="mr-2 h-48 w-80"
       />
       <div className="p-1 m-2">
-        <h3 className="font-bold m-2 gap-3  text 2xl:">{restaurant.name}</h3>
+        <h3 className="font-bold m-2  text 2xl:">{restaurant.name}</h3>
         <div className="flex items-start">
           <div className=" flex mb-2">*****</div>
           <p className="ml-2 text-xsm">77 Reviews</p>
         </div>
         <section className="flex text-reg font-light capitalize mb-2">
           <p className="font-light mr-3">{restaurant.cuisine.name}</p>
-          <p className=" mr-3">{restaurant.price}</p>
+         <Price price={restaurant.price}/>
           <p>{restaurant.location.name}</p>
         </section>
         <p className="text-sm mt-1 font-bold ">
