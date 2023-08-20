a89a033f499d37fb76c9bf1a226e6940df627fcf
Alexandra Macovei <macovei.a2@gmail.com>
2023-08-20 15:05:24 +0200

    [feat ] prop drilling description

--- app/restaurant/[slug]/components/Description.tsx
+++ app/restaurant/[slug]/components/Description.tsx
@@ -1,25 +1,10 @@
 import React from 'react'
 
-export default function Description() {
+export default function Description( {description}: {description:string}) {
   return (
     <div className="mt-4">
     <p className="text-lg font-light mt-4">
-      We believe that good food goes beyond good taste. Nordvegan is
-      not just a vegan food business (even though our food is pretty
-      delicious!), it is a global calling to the people of the world
-      asking them to help protect our beautiful planet while enjoying
-      our equally beautiful food and living a more sustainable
-      lifestyle. Our deli-style restaurant can be found in the local
-      downtown area of Oslo, and serves up healthy plant-based food
-      full of nutrient-dense ingredients. Whether you pop in for lunch
-      or enjoy a hearty dinner, you can enjoy fresh vegan meals from a
-      menu designed by a Michelin-star-trained chef – so you know it
-      will be of the highest quality. As well as proving that vegan
-      food can be tasty as well as nutritious, we also help educate
-      people on how to make constructive changes to eating habits,
-      daily activities and more sustainable living. Stop by today and
-      know you are supporting a true passion project, as well as doing
-      your bit to help avert the global warming crisis.
+      {description}
     </p>
   </div>
   )
