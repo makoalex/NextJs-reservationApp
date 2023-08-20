f46d3b5aa55ee3a3299d794be965175b82d5d346
Alexandra Macovei <macovei.a2@gmail.com>
2023-08-20 15:06:15 +0200

    [feat] prop drilling to display images

--- app/restaurant/[slug]/components/Images.tsx
+++ app/restaurant/[slug]/components/Images.tsx
@@ -1,39 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-export default function Images() {
+export default function Images({ images }: { images: string[] }) {
   return (
     <>
-    <h1 className="font-bold text-lg mt-10 mb-7 border-b pb-7">
-    {' '}
-    5 photos
-  </h1>
-  <div className="flex flex-wrap">
-    <img
-      src="https://resizer.otstatic.com/v2/photos/xlarge/2/46953738.webp"
-      alt=""
-      className="h-44 w-66 mb-1 mr-1"
-    />
-    <img
-      src="https://resizer.otstatic.com/v2/photos/xlarge/2/46953745.webp"
-      alt=""
-      className="h-44 w-66 mb-1 mr-1"
-    />
-    <img
-      src="https://resizer.otstatic.com/v2/photos/xlarge/1/46953727.webp"
-      alt=""
-      className="h-44 w-66 mb-1 mr-1"
-    />
-    <img
-      src="https://resizer.otstatic.com/v2/photos/xlarge/1/46953723.webp"
-      alt=""
-      className="h-44 w-66 mb-1 mr-1"
-    />
-    <img
-      src="https://resizer.otstatic.com/v2/photos/xlarge/1/47389620.webp"
-      alt=""
-      className="h-44 w-66 mb-1 mr-1"
-    />
-  </div>
-  </>
-  )
+      <h1 className="font-bold text-lg mt-10 mb-7 border-b pb-7">
+        {' '}
+        {`${images.length} photo${images.length > 1 ? 's' : ''}`}
+      </h1>
+      <div className="flex flex-wrap">
+        {images.map((image) => (
+          <img className="w-72 h-auto m-3" src={image} alt="" />
+        ))}
+      </div>
+    </>
+  );
 }
