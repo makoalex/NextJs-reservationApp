ff0e53654675cd397a59f3bd67875c2474d144da
Alexandra Macovei <macovei.a2@gmail.com>
2023-08-20 15:05:43 +0200

    [feat] prop drilling to display restaurant name

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header({name}:{name: string}) {
+  const renderTitle = () =>{
+    const nameArray = name.split('-')
+    nameArray[nameArray.length -1] = `(${nameArray[nameArray.length-1]})`
+    return nameArray.join(" ")
+  }
   return (
     <section className="h-96 overflow-hidden">
         <div className=" bg-gradient-to-r  from-[#102310]  to-[#113e21] pt-6 h-full flex justify-center items-center">
           <h1 className="text-7xl text-white capitalize text-shadow text-center font-sans font-thin">
-            Nordvegan (Oslo)
+            {renderTitle()}
           </h1>
         </div>
       </section>
