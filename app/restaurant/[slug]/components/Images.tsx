1ec7cddccf752a78c058387f43d6f417ea0e7c63
Alexandra Macovei <macovei.a2@gmail.com>
2023-08-25 17:09:28 +0200

    [chore] changes to the width  height and margin

--- app/restaurant/[slug]/components/Images.tsx
+++ app/restaurant/[slug]/components/Images.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Images({ images }: { images: string[] }) {
   return (
     <>
-      <h1 className="font-bold text-lg mt-10 mb-7 border-b pb-7">
+      <h1 className="font-bold text-lg mt-7 mb-7 border-b pb-7">
         {' '}
         {`${images.length} photo${images.length > 1 ? 's' : ''}`}
       </h1>
       <div className="flex flex-wrap">
         {images.map((image) => (
-          <img className="w-72 h-72 m-3" src={image} alt="" />
+          <img className="w-64 h-64 m-1 shadow " src={image} alt="" />
         ))}
       </div>
     </>
