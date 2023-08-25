2809a62aa91bd720766b01cc72d39d35c75418f7
Alexandra Macovei <macovei.a2@gmail.com>
2023-08-25 14:41:45 +0200

    [feat]fetching data by location, will be adding mor cities to the database later

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,8 +4,39 @@ import NavBar from '../components/NavBar';
 import Header from './components/Header';
 import SearchSideBar from './components/SearchSideBar';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient } from '@prisma/client';
 
-export default function Search() {
+const prisma = new PrismaClient();
+
+const fecthRestaurantsByCity = (city: string |undefined) => {
+  if(!city) return prisma.restaurant.findMany()
+  return  prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city,
+        },
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      mainImage: true,
+      price: true,
+      cuisine: true,
+      location: true,
+      slug:true,   
+    },
+  });
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city: string };
+}) {
+  const restaurants = await fecthRestaurantsByCity(searchParams.city)
+
   return (
     <>
       {/* Header */}
